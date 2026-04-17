354dfefc58b45e7c6d2301f51539fecfe1f56ca5
KOSASIH <kosasihg88@gmail.com>
2026-04-17 21:39:45 +0700

    Create abi.js

---
+++ frontend/src/abi.js
@@ -0,0 +1,6 @@
+export const RPI_ABI = [
+  "function balanceOf(address) view returns (uint256)",
+  "function mint(address,uint256)",
+  "function approve(address,uint256)",
+  "function transfer(address,uint256)"
+];
